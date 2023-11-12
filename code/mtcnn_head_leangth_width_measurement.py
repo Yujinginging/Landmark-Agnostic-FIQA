0791a36ba0400db63e85201cd385567cf7f90001
Teakosheen <teakojolak@yahoo.com>
2023-11-12 16:29:22 +0100

    added head length from crown to mtcnn + retinaFace

--- code/mtcnn_head_leangth_width_measurement.py
+++ code/mtcnn_head_leangth_width_measurement.py
@@ -1,5 +1,6 @@
 from mtcnn import MTCNN
 import cv2
+import math
 
 
 #load MTCNN detector
@@ -18,13 +19,36 @@ for face in faces:
     x, y, w, h = face['box'] #x,y:top-left corner coordinates
     head_length = h  # Height of the rectangle is considered head length
     head_width = w  # Width of the rectangle is considered head width
+    eye1_x, eye1_y = face['keypoints']['left_eye'] # left eye coordinates
+    eye2_x, eye2_y = face['keypoints']['right_eye'] # right eye coordinates
+
+    #calculate the center coordinates between the eyes
+    center_x = (eye1_x + eye2_x) // 2
+    center_y = (eye1_y + eye2_y) // 2
+    center_eye_coordinate = (center_x, center_y)
+
+    # calculate the the  chin coordinates 
+    chin_x = x + w // 2
+    chin_y = y + h 
+    chin_coordinate = (chin_x, chin_y) 
+
+    # claculate the distance between the chin and the calculated centeral dot coordinates between the eyes
+    distance = math.sqrt((center_eye_coordinate[0] - chin_coordinate[0]) ** 2 + (center_eye_coordinate[1] - chin_coordinate[1]) ** 2)
+
+    # calculate the head length from the crown to the chin 
+    head_length_from_crown = 2 * distance
 
     # Draw the rectangle on the image (for visualization only)
-    cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+    #cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+    # Draw the line between the center of the eyes and the chain
+    head_crown_y = (center_y) -  round(distance) # claculate the crown y coordinates
+    cv2.line(image, (chin_x, head_crown_y), (chin_x, chin_y), (0, 255, 0), 2)
     
     # Print the head width and length in pixels
     print(f"Head Width: {head_width} pixels")
     print(f"Head Length: {head_length} pixels")
+    print(f"Head Length from crown: {head_length_from_crown} pixels")
+
     
     #save the image with rectangles: for test and view results later
     cv2.imwrite('/home/teakoo/Landmark-Agnostic-FIQA/img_test/test_output/518_withRect.jpg',image)
--- code/retinaface_head_leangth_width_measurement.py
+++ code/retinaface_head_leangth_width_measurement.py
@@ -1,5 +1,6 @@
 from retinaface import RetinaFace
 import cv2
+import math
 
 
 #load and preprocess image:
@@ -18,16 +19,45 @@ for face, value in faces.items():
             head_length = h - y  # calculate face length
             head_width = w - x # calculate face Width 
 
+            # calculate the the  chin coordinates 
+            chin_x = w  // 2
+            chin_y = h 
+            chin_coordinate = (round(chin_x), round(chin_y)) 
+
             # Draw the rectangle on the image (for visualization only)
-            cv2.rectangle(image, (x, y), (w, h), (0, 255, 0), 2)
+            #cv2.rectangle(image, (x, y), (w, h), (0, 255, 0), 2)
     
             # Print the head width and length in pixels
             print(f"Head Width: {head_width} pixels")
             print(f"Head Length: {head_length} pixels")
     
             #save the image with rectangles: for test and view results later
+            #cv2.imwrite('/home/teakoo/Landmark-Agnostic-FIQA/img_test/test_output/518_withRect.jpg',image)
+        if cord == "landmarks":
+            eye1_x, eye1_y = values['left_eye'] # left eye coordinates
+            eye2_x, eye2_y = values['right_eye'] # right eye coordinates
+
+            #calculate the center coordinates between the eyes
+            center_x = (eye1_x + eye2_x) // 2
+            center_y = (eye1_y + eye2_y) // 2
+            center_eye_coordinate = (round(center_x), round(center_y))
+
+            # claculate the distance between the chin and the calculated centeral dot coordinates between the eyes
+            distance = math.sqrt((center_eye_coordinate[0] - chin_coordinate[0]) ** 2 + (center_eye_coordinate[1] - chin_coordinate[1]) ** 2)
+
+            # calculate the head length from the crown to the chin 
+            head_length_from_crown = 2 * distance
+
+            # Draw the line between the center of the eyes and the chain
+            head_crown_y = round(center_y) -  round(distance) # claculate the crown y coordinates
+            cv2.line(image, (chin_x, head_crown_y), (chin_x, chin_y), (0, 255, 0), 2)
+
+            # Print the head width and length in pixels
+            print(f"Head Length from crown: {round(head_length_from_crown)} pixels")
+
+            #save the image with rectangles: for test and view results later
             cv2.imwrite('/home/teakoo/Landmark-Agnostic-FIQA/img_test/test_output/518_withRect.jpg',image)
-    
+
 
 # Display or save the image with rectangles (optional)
 cv2.imshow('Image with Rectangles', image)
