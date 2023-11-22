d4ee2b3939c8f6579434222f903f874779ab2c6a
Yujinginging <s220420@student.dtu.dk>
2023-11-22 15:16:58 +0100

    resize imgs

--- code/pic_resolution.py
+++ code/pic_resolution.py
@@ -1,17 +1,31 @@
 # Import the Images module from pillow 
-from PIL import Image 
+from PIL import Image
+import cv2 
 
 # Open the image by specifying the image path. 
-image_path = '/home/jing/FIQA_repo/face_parsing.PyTorch/res/img/568.jpg'
-image_file = Image.open(image_path) 
+image_path = '/home/jing/FIQA_repo/img_test/518.jpg'
+image_file = Image.open(image_path)
 
-# the default 
-image_file.save('/home/jing/FIQA_repo/face_parsing.PyTorch/res/img/568_0.jpg', quality=95) 
-
-# Changing the image resolution using quality parameter 
-# Example-1 
-image_file.save('/home/jing/FIQA_repo/face_parsing.PyTorch/res/img/568_1.jpg', quality=25) 
-
-# Example-2 
-image_file.save('/home/jing/FIQA_repo/face_parsing.PyTorch/res/img/568_2.jpg', quality=10) 
-print('ok')
+# get the width and height of picture
+width, height = Image.open(image_path).size
+width1 = round(width * 3/4 )
+height1 = round(height * 3/4)
+new_image = image_file.resize((width1, height1))
+new_image.save('/home/jing/FIQA_repo/img_test/518_1.jpg')
+img1 =cv2.imread('/home/jing/FIQA_repo/img_test/518_1.jpg')
+print('Image Width is',img1.shape[1])
+print('Image Height is',img1.shape[0])
+width2 = round(width * 1/2 )
+height2 = round(height * 1/2)
+new_image = image_file.resize((width2, height2))
+new_image.save('/home/jing/FIQA_repo/img_test/518_2.jpg')
+img1 =cv2.imread('/home/jing/FIQA_repo/img_test/518_2.jpg')
+print('Image Width is',img1.shape[1])
+print('Image Height is',img1.shape[0])
+width3 = round(width * 1/4 )
+height3 = round(height * 1/4)
+new_image = image_file.resize((width3, height3))
+new_image.save('/home/jing/FIQA_repo/img_test/518_3.jpg')
+img1 =cv2.imread('/home/jing/FIQA_repo/img_test/518_3.jpg')
+print('Image Width is',img1.shape[1])
+print('Image Height is',img1.shape[0])
